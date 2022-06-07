05dc79d9e5788e247dfcbe2be368c57212ca1a6c
kolynzb <collinsbenda360@gmail.com>
2022-06-07 10:57:10 +0300

    Configure Test Environment

--- api/__tests__/sample.test.js
+++ api/__tests__/sample.test.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 function sum(a, b) {
   return a + b;
 }
 
-test("adds 1 + 2 to equal 3", () => {
+describe("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
